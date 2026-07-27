6ad9c4dcb242e1ba594bae923c0638ab87132b89
Jb-umsunguli <jamesharrington.startrt@gmail.com>
2026-07-27 17:22:46 -0400

    Update urcard-checkout.mjs

--- urcard-checkout.mjs
+++ urcard-checkout.mjs
@@ -4060,8 +4060,9 @@ function pos_inputTokenization() {
 // const markers = new Map();
 // import { Loader } from "@googlemaps/js-api-loader";
 // const loader = new Loader({
-//     apiKey: "AIzaSyDYWdF5Y0PyquMmIVeynOLmLUXRXBNeNe4",
-//     version: "weekly"
+//     apiKey: "AIzaSyDYWdF5Y0PyquMmIVeynOLmLUXRXBNeNe4" 
+//     // apiKey: "AIzaSyDYWdF5Y0PyquMmIVeynOLmLUXRXBNeNe4",
+//     // version: "weekly"
 // });
 
 // async function initMap() {
@@ -4144,4 +4145,84 @@ function pos_inputTokenization() {
 // }
 
 // initMap();
+
+
+
+
+
+
+
+
+      "use strict";
+
+      // This loads helper components from the Extended Component Library,
+      // https://github.com/googlemaps/extended-component-library.
+      import {APILoader} from 'https://ajax.googleapis.com/ajax/libs/@googlemaps/extended-component-library/0.6.15/index.min.js';
+
+      const CONFIGURATION = {
+        "ctaTitle": "Checkout",
+        "mapOptions": {"center":{"lat":37.4221,"lng":-122.0841},"fullscreenControl":false,"mapTypeControl":false,"streetViewControl":false,"zoom":11,"zoomControl":false,"maxZoom":22,"mapId":""},
+        "mapsApiKey": "AIzaSyDYWdF5Y0PyquMmIVeynOLmLUXRXBNeNe4",
+        "capabilities": {"addressAutocompleteControl":true,"mapDisplayControl":false,"ctaControl":true}
+      };
+
+      const SHORT_NAME_ADDRESS_COMPONENT_TYPES =
+          new Set(['street_number', 'administrative_area_level_1', 'postal_code']);
+
+      const ADDRESS_COMPONENT_TYPES_IN_FORM = [
+        'location',
+        'locality',
+        'administrative_area_level_1',
+        'postal_code',
+        'country',
+      ];
+
+      function getFormInputElement(componentType) {
+        return document.getElementById(`${componentType}-input`);
+      }
+
+      function fillInAddress(place) {
+        function getComponentName(componentType) {
+          for (const component of place.address_components || []) {
+            if (component.types[0] === componentType) {
+              return SHORT_NAME_ADDRESS_COMPONENT_TYPES.has(componentType) ?
+                  component.short_name :
+                  component.long_name;
+            }
+          }
+          return '';
+        }
+
+        function getComponentText(componentType) {
+          return (componentType === 'location') ?
+              `${getComponentName('street_number')} ${getComponentName('route')}` :
+              getComponentName(componentType);
+        }
+
+        for (const componentType of ADDRESS_COMPONENT_TYPES_IN_FORM) {
+          getFormInputElement(componentType).value = getComponentText(componentType);
+        }
+      }
+
+      async function initMap() {
+        const {Autocomplete} = await APILoader.importLibrary('places');
+
+        const autocomplete = new Autocomplete(getFormInputElement('location'), {
+          fields: ['address_components', 'geometry', 'name'],
+          types: ['address'],
+        });
+
+        autocomplete.addListener('place_changed', () => {
+          const place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert(`No details available for input: '${place.name}'`);
+            return;
+          }
+          fillInAddress(place);
+        });
+      }
+
+      initMap();
 //  //END - ADDRESS SEARCH ASSETS
