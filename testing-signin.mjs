a4fed449fafff8cb6f1b917014cc291faefc4c27
Jb-umsunguli <jamesharrington.startrt@gmail.com>
2026-07-31 00:35:28 -0400

    Update testing-signin.mjs

--- testing-signin.mjs
+++ testing-signin.mjs
@@ -1,1 +1,13 @@
-const 
+const testing_signin_input = document.getElementById("testing-signin-input-id");
+const testing_signin_btn = document.getElementById("testing-signin-btn-id"); 
+testing_signin_btn.addEventListener('click', () => {
+  if (testing_signin_input.value == "") {
+    //DO NOTHING
+  } else {
+    if (testing_signin_input.value == "run_checkout_test") {
+      
+    } else {
+      //DO NOTHING
+    }
+  }
+})
